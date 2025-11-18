a4a45626f2fd43a100e401a6950ea95145a30a1c
morocho <portafoliomoro@gamil.com>
2025-11-17 21:09:42 -0500

    Feat(modelo:Deportista): Construir la clase Deportista de la capa de modelo

---
+++ src/main/java/gm/Ajedrez/modelo/Deportista.java
@@ -0,0 +1,22 @@
+package gm.Ajedrez.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Deportista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+}
