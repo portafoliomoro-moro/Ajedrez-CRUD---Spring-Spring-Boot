224a34e49fadc0528cececfbddae6cf3d777daef
morocho <portafoliomoro@gamil.com>
2025-11-27 12:51:52 -0500

    Feat(swing): Integrar Swing al proyecto

--- src/main/java/gm/Ajedrez/AjedrezApplication.java
+++ src/main/java/gm/Ajedrez/AjedrezApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AjedrezApplication implements CommandLineRunner {
 
 	@Autowired
